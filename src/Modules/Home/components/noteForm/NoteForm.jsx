a67d7faa6d658747f037bcb4252dcd906a179a81
Jheff <divegas43@gmail.com>
2024-12-14 21:43:27 -0500

    Updated: api url

--- src/Modules/Home/components/noteForm/NoteForm.jsx
+++ src/Modules/Home/components/noteForm/NoteForm.jsx
@@ -11,9 +11,17 @@ import { toast } from 'react-toastify';
 import { MdDriveFileRenameOutline } from 'react-icons/md';
 import { CONTRADICTORY_TAGS, TAG_OPTIONS } from '../../../shared/const';
 
-export const NoteForm = ({ setLoadNotes, loadNotes, currentNote, userId, setCurrentNote }) => {
+export const NoteForm = ({
+  setLoadNotes,
+  loadNotes,
+  currentNote,
+  userId,
+  setCurrentNote,
+}) => {
   const [title, setTitle] = useState('');
-  const tagsArray = currentNote?.tags?.length ? currentNote?.tags.split(',') : [];
+  const tagsArray = currentNote?.tags?.length
+    ? currentNote?.tags.split(',')
+    : [];
   const [tags, setTags] = useState([]);
   const contentRef = useRef('');
 
@@ -26,7 +34,7 @@ export const NoteForm = ({ setLoadNotes, loadNotes, currentNote, userId, setCurr
       title,
       content: contentRef.current.getContent(),
       userId,
-      tags: tags.map(tag => tag.value).join(',')
+      tags: tags.map((tag) => tag.value).join(','),
     };
 
     try {
@@ -45,13 +53,17 @@ export const NoteForm = ({ setLoadNotes, loadNotes, currentNote, userId, setCurr
   };
   useEffect(() => {
     setTitle(currentNote?.title ?? '');
-    setTags(tagsArray ? tagsArray.map(tag => ({ label: tag, value: tag })) : []);
+    setTags(
+      tagsArray ? tagsArray.map((tag) => ({ label: tag, value: tag })) : [],
+    );
   }, [currentNote]);
 
   const updateDisabledOptions = (selectedTags) => {
-    const updatedOptions = TAG_OPTIONS.map(option => {
+    const updatedOptions = TAG_OPTIONS.map((option) => {
       const contradictedTags = CONTRADICTORY_TAGS[option.value] || [];
-      const isDisabled = contradictedTags.some(tag => selectedTags.some(selected => selected.value === tag));
+      const isDisabled = contradictedTags.some((tag) =>
+        selectedTags.some((selected) => selected.value === tag),
+      );
       return { ...option, disabled: isDisabled };
     });
     return updatedOptions;
@@ -59,19 +71,21 @@ export const NoteForm = ({ setLoadNotes, loadNotes, currentNote, userId, setCurr
 
   return (
     <div className="note-form-container">
-      <div className='d-flex justify-content-center mb-3'>
+      <div className="d-flex justify-content-center mb-3">
         <h1>{currentNote ? 'Edit note' : 'Add note'}</h1>
       </div>
 
-      <form className='d-flex flex-column gap-2'>
+      <form className="d-flex flex-column gap-2">
         <div className="form-field-login">
-          <label htmlFor="title" className='input-label mb-3'>Title</label>
-          <div className='d-flex align-items-center'>
-            <MdDriveFileRenameOutline className='field-icon' />
+          <label htmlFor="title" className="input-label mb-3">
+            Title
+          </label>
+          <div className="d-flex align-items-center">
+            <MdDriveFileRenameOutline className="field-icon" />
             <input
               type="text"
               id="title"
-              className='w-100 input-with-icon'
+              className="w-100 input-with-icon"
               value={title}
               onChange={(e) => setTitle(e.target.value)}
             />
@@ -79,7 +93,9 @@ export const NoteForm = ({ setLoadNotes, loadNotes, currentNote, userId, setCurr
         </div>
 
         <div className="form-field-login position-relative">
-          <label htmlFor="tags" className='input-label mb-3'>Tags</label>
+          <label htmlFor="tags" className="input-label mb-3">
+            Tags
+          </label>
           <MultiSelect
             options={updateDisabledOptions(tags)}
             value={tags}
@@ -92,30 +108,42 @@ export const NoteForm = ({ setLoadNotes, loadNotes, currentNote, userId, setCurr
         </div>
 
         <div className="form-field-login position-relative">
-          <label htmlFor="content" className='input-label mb-3'>Content</label>
-          <div className='d-flex align-items-center password'>
+          <label htmlFor="content" className="input-label mb-3">
+            Content
+          </label>
+          <div className="d-flex align-items-center password">
             <div className="editor-container">
               <Editor
-                tinymceScriptSrc='node_modules/tinymce/tinymce.min.js'
-                onInit={(_evt, editor) => contentRef.current = editor}
+                tinymceScriptSrc="node_modules/tinymce/tinymce.min.js"
+                onInit={(_evt, editor) => (contentRef.current = editor)}
                 initialValue={currentNote?.content ?? ''}
                 // apiKey='zihkwon6rwrhylq751dpeas0uczb7nflfp8jnnzfom1lbxkl'
+
                 init={{
-                  height: "250px",
-                  skin: "oxide-dark",
-                  content_css: "dark",
-                  width: "100%",
-                  plugins: 'anchor autolink charmap codesample emoticons image link lists media searchreplace table visualblocks wordcount ',
-                  toolbar: 'undo redo | blocks fontfamily fontsize | bold italic underline strikethrough | link image media table | align lineheight | numlist bullist indent outdent | emoticons charmap | removeformat',
+                  license_key: 'gpl',
+                  height: '250px',
+                  skin: 'oxide-dark',
+                  content_css: 'dark',
+                  width: '100%',
+                  plugins:
+                    'anchor autolink charmap codesample emoticons image link lists media searchreplace table visualblocks wordcount ',
+                  toolbar:
+                    'undo redo | blocks fontfamily fontsize | bold italic underline strikethrough | link image media table | align lineheight | numlist bullist indent outdent | emoticons charmap | removeformat',
                 }}
               />
             </div>
           </div>
         </div>
 
-        <div className='mt-2 d-flex row'>
-          <div className='col'>
-            <Button variant="primary" onClick={handleManageNote} className='w-100 rounded-4'>{currentNote ? 'Update note' : 'Save note'}</Button>
+        <div className="mt-2 d-flex row">
+          <div className="col">
+            <Button
+              variant="primary"
+              onClick={handleManageNote}
+              className="w-100 rounded-4"
+            >
+              {currentNote ? 'Update note' : 'Save note'}
+            </Button>
           </div>
         </div>
       </form>
